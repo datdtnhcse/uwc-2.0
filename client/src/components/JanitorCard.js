2e7a535a21861ba6dccbabfba66613215678cf18
svn <81409431+sangnguyencs20@users.noreply.github.com>
2022-12-14 05:19:40 +0700

    Janitor Collapse Done

--- client/src/components/JanitorCard.js
+++ client/src/components/JanitorCard.js
@@ -1,7 +1,22 @@
-export default function JanitorCard({ item }) {
+// {
+//     "id": 1,
+//     "name": "Le Tri Minh",
+//     "nearest-mcp": "MCP#6",
+//     "status": 1
+// },
+export default function JanitorCard({ data }) {
     return (
-        <div>
-            <h1>{item.id}</h1>
+        <div class="col-md-6 col-xs-12 col-xl-4 mb-2 ">
+            <div class="card">
+                <div class="card-body">
+                    <h5 class="card-title">{data.name}</h5>
+                    {
+                        data.status === 1 ? <p class="card-text">Status: <span class="badge bg-success">Available</span></p> : <p class="card-text">Status: <span class="badge bg-danger">Unavailable</span></p>
+                    }
+                    <h5>Nearest MCP: {data.nearest_mcp}</h5>
+                    <a href="#" class="btn btn-primary">Assign</a>
+                </div>
+            </div>
         </div>
     )
 }
