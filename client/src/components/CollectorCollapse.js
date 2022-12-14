443bfd125a9fd17ed06304cccfa0395f0f926eba
haitrancs <tranhai.tt49@gmail.com>
2022-12-14 16:31:59 +0700

    Update UI
    change button color

--- client/src/components/CollectorCollapse.js
+++ client/src/components/CollectorCollapse.js
@@ -1,6 +1,10 @@
 import CollectorCard from "./CollectorCard";
 import data from "../mockup_data/collector.json"
+
 export default function CollectorCollapse() {
+    data = data.sort(function(a, b){
+        return b.status - a.status
+    })
     return (
         <div className="row d-flex justify-content-between">
             {
