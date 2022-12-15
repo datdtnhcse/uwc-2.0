1c5a2c30f14665bef31c8f632fedeeb58a0ca50f
Leino <lecongcuong2604@gmail.com>
2022-12-15 21:52:34 +0700

    Update CreateRoute.js

--- client/src/components/CreateRoute.js
+++ client/src/components/CreateRoute.js
@@ -1,59 +1,89 @@
-//Get MCPs for route selection
-
 import data from "../mockup_data/overviewMCP.json";
 import depotData from "../mockup_data/overviewDepot.json";
-import GTCData from "../mockup_data/overviewGTC.json"
+import GTCData from "../mockup_data/overviewGTC.json";
 import { Form, Button, Collapse, ListGroup } from "react-bootstrap";
 import { useState, useEffect, useRef } from "react";
+
+const api = "http://localhost:3001/routedata/add";
 // import { List } from "react-bootstrap-icons";
 
-function AddRoute () {
-    
-}
+// {
+//     "name": "HCMUT Company",
+//     "from": "Depot3",
+//     "to": {
+//         "id": 3,
+//         "name": "SAD"
+//     },
+//     "mcps": [1,3,4,5],
+//     "status": "1"
+// }
 
 export default function CreateRoute() {
-    // var MCParray = [];
-    // var GTC = 1;
-    var depot = "";
-
     const [open, setOpen] = useState(false);
 
-    const [newRoute, setNewRoute] = useState({});
-    const MCParray = useRef([]);
     const [routeName, setRouteName] = useState("");
+    const [fromDepot, setFromDepot] = useState("");
+    const [toGTC, setToGTC] = useState({
+        id: 0,
+        name: "",
+    });
+    const [mcps, setMcps] = useState([]);
+    var status = "0";
 
-    const onMCPadd = (ele) => {
-        MCParray.push(ele.id);
+    const handleMcps = (e) => {
+        e.preventDefault();
+        const mcpValue = e.target.value;
+        const prevMcps = mcps;
+        setMcps([...prevMcps, mcpValue]);
+        console.log(mcps);
     };
 
+    const handleToGTC = (id, name) => {
+        setToGTC({
+            id: id,
+            name: name,
+        });
+        console.log(toGTC);
+    };
+
+    //add route
     const handleSubmit = (e) => {
-        // e.preventDefault();
-        // setNewRoute((values) => [
-        //     ...values,
-        //     {
-        //         routeName: routeName,
-        //         fromDepot: depot,
-        //         toGTC: {
-        //             GTCID: 1,
-        //             GTCName: "GTC1",
-        //         },
-        //         routeOfMCPsID: MCParray,
-        //         status: 0,
-        //     },
-        // ]);
-    };
+        e.preventDefault();
+        console.log(routeName, fromDepot, toGTC, mcps, status);
 
-    const onaddMCP = (ele) => {
-        // MCParray.current.push(ele);
-        // console.log(ele);
-        // setMCParray((arr) => [...arr, ele])
+        //Method Add
+        async function addRoute() {
+            let response = await fetch(api, {
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json",
+                },
+
+                //name, from, to{id, name}, mcps, status
+                body: JSON.stringify({
+                    name: routeName,
+                    from: fromDepot,
+                    to: toGTC,
+                    mcps: mcps,
+                    status,
+                }),
+            });
+            response = await response.json();
+            console.log(typeof response);
+            // setData(response)
+        }
+        addRoute();
     };
 
     const handleReset = (e) => {
         e.preventDefault();
-        MCParray = [];
-        setNewRoute({});
         setRouteName("");
+        setFromDepot("");
+        setToGTC({
+            id: 0,
+            name: "",
+        });
+        setMcps([]);
     };
 
     return (
@@ -65,6 +95,7 @@ export default function CreateRoute() {
             >
                 Create Route
             </Button>
+
             <Form onSubmit={handleSubmit} onReset={handleReset}>
                 <Collapse in={open}>
                     <div className="create-route-collapse">
@@ -73,9 +104,10 @@ export default function CreateRoute() {
                             {data.length > 0 ? (
                                 data.map((item, i) => (
                                     <ListGroup.Item
-                                        // action
-                                        // onClick={onaddMCP(item.id)}
+                                        action
+                                        onClick={handleMcps}
                                         key={i}
+                                        value={item.id}
                                     >
                                         {item.id}
                                     </ListGroup.Item>
@@ -89,9 +121,14 @@ export default function CreateRoute() {
                             {depotData.length > 0 ? (
                                 depotData.map((item, i) => (
                                     <ListGroup.Item
-                                        // action
-                                        // onClick={(depot = item.depotName)}
+                                        action
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            setFromDepot(item.depotName);
+                                            console.log(fromDepot);
+                                        }}
                                         key={i}
+                                        value={item.depotName}
                                     >
                                         {item.depotName}
                                     </ListGroup.Item>
@@ -105,15 +142,18 @@ export default function CreateRoute() {
                             {GTCData.length > 0 ? (
                                 GTCData.map((item, i) => (
                                     <ListGroup.Item
-                                        // action
-                                        // onClick={(depot = item.depotName)}
+                                        action
+                                        onClick={(e) => {
+                                            e.preventDefault();
+                                            handleToGTC(item.id, item.name);
+                                        }}
                                         key={i}
                                     >
                                         <div>{item.id}</div>
                                     </ListGroup.Item>
                                 ))
                             ) : (
-                                <div>No depot</div>
+                                <div>No GTC</div>
                             )}
                         </ListGroup>
                         <br></br>
@@ -123,7 +163,10 @@ export default function CreateRoute() {
                             type="text"
                             id="routeName"
                             value={routeName}
-                            onChange={(e) => setRouteName(e.target.value)}
+                            onChange={(e) => {
+                                setRouteName(e.target.value);
+                                console.log(routeName);
+                            }}
                         />
 
                         <div>
