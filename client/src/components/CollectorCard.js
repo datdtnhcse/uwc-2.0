8238caa99e17de4d1f149a8e68639cbe43da1ebc
haitrancs <tranhai.tt49@gmail.com>
2022-12-15 10:11:39 +0700

    Update CollectorCard.js

--- client/src/components/CollectorCard.js
+++ client/src/components/CollectorCard.js
@@ -19,7 +19,7 @@ export default function CollectorCard({ data }) {
                     <h5>ID: {data.id}</h5>
                     <h5>Nearest Depot: {data.nearest_depot}</h5>
                     {
-                        data.status === 1 ? <AssignModal /> : <Button className="mybtn"> Assigned </Button>
+                        data.status === 1 ? <AssignModal /> : <Button className="abtn"> Assigned </Button>
                     }
                 </div>
             </div>
